0ddafbed390971fc5d4d579be940798ae6787bf9
almond <dhkim20157@gmail.com>
2016-11-03 19:41:17 +0900

    선생님만 방 만들 수 있도록 패치

--- class_room/views.py
+++ class_room/views.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import Class
@@ -6,25 +8,47 @@ from accounts.models import CustumUser
 
 # Create your views here.
 def index(request):
+
     try:
+
+        if request.GET.get('error') == '1':
+            print('error')
+            error = {
+                'error': True,
+                'message': '선생님만 그룹을 생성할 수 있습니다.'
+            }
+
+        else:
+            print('not error')
+            error = {
+                'error': False,
+                'message': ''
+            }
+
         user = CustumUser.objects.get(username=request.user)
-        return render(request, 'class_room/index.html', {'user' : user})
+        return render(request, 'class_room/index.html', {'user': user, 'error': error})
+
     except:
         return HttpResponseRedirect('accounts/login')
 
 
 def make_group(request):
+    user = CustumUser.objects.get(username=request.user)
     form = Class(code=request.POST.get('code'), name=request.POST.get('name'))
 
-    if request.method == 'POST':
-        newClass = form.save()
+    if user.job == 'teacher':
+        if request.method == 'POST':
+            newClass = form.save()
+
+        else:
+            form = Class()
+
+        group = Class.objects.filter().order_by('name')
+
+        return render(request, 'class_room/make_group.html', {'form' : form, 'groupCnt' : len(group)})
 
     else:
-        form = Class()
-
-    group = Class.objects.filter().order_by('name')
-
-    return render(request, 'class_room/make_group.html', {'form' : form, 'groupCnt' : len(group)})
+        return HttpResponseRedirect('/?error=1')
 
 def join_group(request):
     if request.method == 'POST':

81cfe79a034af9f34c9dbba06e0fc3aaa1a79081
almond <dhkim20157@gmail.com>
2016-11-10 16:44:11 +0900

    resetting

--- class_room/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Class, Post, Comment
-
-# Register your models here.
-admin.site.register(Class)
-admin.site.register(Post)
-admin.site.register(Comment)
--- class_room/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class ClassRoomConfig(AppConfig):
-    name = 'class_room'
